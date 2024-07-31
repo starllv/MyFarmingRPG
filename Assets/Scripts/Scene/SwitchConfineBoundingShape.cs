426ed7834de3a7d18d66da148fd11a4fe32e868d
starllv <zhangqin37@aliyun.com>
2024-07-31 16:14:40 +0800

    为场景增加树，并设置人物跟踪相机的显示范围，不超出地图边界

---
+++ Assets/Scripts/Scene/SwitchConfineBoundingShape.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using Cinemachine;
+
+public class SwitchConfineBoundingShape : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchBoundingShape();
+    }
+
+    // 切换cinemachine用来定义屏幕边界的碰撞器
+    private void SwitchBoundingShape()
+    {
+        PolygonCollider2D polygonCollider2D = GameObject.FindGameObjectWithTag(Tags.BoundsConfiner).GetComponent<PolygonCollider2D>();
+
+        CinemachineConfiner cinemachineConfiner = GetComponent<CinemachineConfiner>();
+
+        cinemachineConfiner.m_BoundingShape2D = polygonCollider2D;
+
+        cinemachineConfiner.InvalidatePathCache();
+    }
+}
